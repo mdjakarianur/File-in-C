582fb2fb238d1442e98b822067a99c7911d8c516
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-29 23:49:31 +0600

    Update 01 File Operation.c

--- 01 File Operation.c
+++ 01 File Operation.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
-int main()
-{
+int main(){
  FILE *fp;
  char filename[]="my_file.txt";
+ 
  fp=fopen(filename,"w");
+ 
  fprintf(fp,"We are learning file operations in c\n");
  fprintf(fp,"Hello People\n");
+ 
  fclose(fp);
+ 
  fp=fopen(filename,"a");
+ 
  fprintf(fp,"Hello World\n");
  return 0;
 }
